f3e7cfc09b921b6ed3f218e446f9837906ee02cc
Rahul Sharma <rahusharma.sharma@gmail.com>
2023-02-12 21:11:23 +0530

    added palindrome

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str = "nayan";
+let bag = ';
+
+for (let i = str.length - 1; i >= 0; i++) {
+  bag += str[i];
+}
+if (bag == str)
+  console.log("palindrome")
+else {
+  console.log("Not palindrome")
+}
